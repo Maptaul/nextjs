a4a7d224ed9d411cbcd331f71d1da7db646ac5c4
Maptaul <maptaul912@gmail.com>
2025-08-24 23:49:19 +0600

    Add Dynamic Product Page component

---
+++ src/app/products/[productid]/page.jsx
@@ -0,0 +1,12 @@
+const DynamicProductPage = () => {
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center">
+      <h1 className="text-4xl font-bold">Dynamic Product Name</h1>
+      <p className="mt-4 text-lg">
+        Dynamic product details will be displayed here.
+      </p>
+    </div>
+  );
+};
+
+export default DynamicProductPage;
